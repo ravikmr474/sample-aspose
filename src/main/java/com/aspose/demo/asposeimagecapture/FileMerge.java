f36b215c9fe4c7b58bf8d6f99aa4ae46cdf257d8
ravikmr474 <40664321+ravikmr474@users.noreply.github.com>
2023-03-14 20:03:56 +0530

    File merge updated

--- src/main/java/com/aspose/demo/asposeimagecapture/FileMerge.java
+++ src/main/java/com/aspose/demo/asposeimagecapture/FileMerge.java
@@ -5,6 +5,7 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -64,10 +65,10 @@ public class FileMerge {
 								if (width > 72 * 6.5)
 									scaling = (72 * 6.5) / width;
 								bout = new ByteArrayOutputStream();
-								ImageIO.write(image, "jpeg", bout);
+								ImageIO.write(image, "png", bout);
 								bout.flush();
 								bin = new ByteArrayInputStream(bout.toByteArray());
-								r.addPicture(bin, XWPFDocument.PICTURE_TYPE_JPEG, "", Units.toEMU(width*scaling),
+								r.addPicture(bin, XWPFDocument.PICTURE_TYPE_PNG, "", Units.toEMU(width*scaling),
 										Units.toEMU(height*scaling));
 								r.getCTR().getDrawingArray(0).getInlineArray(0).addNewCNvGraphicFramePr()
 										.addNewGraphicFrameLocks().setNoChangeAspect(true);
@@ -80,6 +81,7 @@ public class FileMerge {
 			}
 		} catch (Exception e) {
 			System.out.println("Exception: " + e);
+			e.printStackTrace();
 		}
 		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 		doc.write(outStream);
@@ -99,63 +101,109 @@ public class FileMerge {
 		Map<String, OutputStream> imageOutputStreamMap = new HashMap<String, OutputStream>();
 		try {
 
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			workbookFile.write(bos);
-			byte[] barray = bos.toByteArray();
-			InputStream is = new ByteArrayInputStream(barray);
-
-			Workbook workbook = new Workbook(is);
+//			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//			workbookFile.write(bos);
+//			byte[] barray = bos.toByteArray();
+//			InputStream is = new ByteArrayInputStream(barray);
+//
+//			Workbook workbook = new Workbook(is);
+//			FontConfigs.setDefaultFontName("Arial");
+//			workbook.calculateFormula();
+//			Style defaultStyle = workbook.getDefaultStyle();
+//			FontSettings fs = FontSettings.getDefaultInstance();
+//			fs.setFontsSources(
+//					new FontSourceBase[] { new SystemFontSource(), new FolderFontSource("/usr/share/fonts", true) });
+//
+//			defaultStyle.getFont().setName("Arial");
+//			defaultStyle.getFont().setSize(10);
+//			defaultStyle.setTextWrapped(true);
+//			defaultStyle.getFont().setBold(false);
+//			workbook.setDefaultStyle(defaultStyle);
+//			WorksheetCollection worksheets = workbook.getWorksheets();
+//
+//			int sheetCount = worksheets.getCount();
+			
+			Workbook workbook = new Workbook("C:\\Users\\RaviKumar(JAI)\\OneDrive - Formidium Corp\\Desktop\\My-excel.xlsx");
 			FontConfigs.setDefaultFontName("Arial");
 			workbook.calculateFormula();
 			Style defaultStyle = workbook.getDefaultStyle();
-			FontSettings fs = FontSettings.getDefaultInstance();
-			fs.setFontsSources(
-					new FontSourceBase[] { new SystemFontSource(), new FolderFontSource("/usr/share/fonts", true) });
 
 			defaultStyle.getFont().setName("Arial");
 			defaultStyle.getFont().setSize(10);
 			defaultStyle.setTextWrapped(true);
 			defaultStyle.getFont().setBold(false);
 			workbook.setDefaultStyle(defaultStyle);
-			WorksheetCollection worksheets = workbook.getWorksheets();
-
-			int sheetCount = worksheets.getCount();
-			for (int index = 0; index < sheetCount; index++) {
-				Worksheet workbookName = workbook.getWorksheets().get(index);
-				String name = workbookName.getName();
-				Range range = workbookName.getCells().getMaxDisplayRange();
-				if (range != null) {
-					int tcols = range.getColumnCount();					
-					ImageOrPrintOptions options = new ImageOrPrintOptions();
-					options.setOnePagePerSheet(true);
-					options.setImageType(ImageType.JPEG);
-					options.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-					options.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-							RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-					options.setHorizontalResolution(200);
-					options.setVerticalResolution(200);
-					options.setTiffCompression(TiffCompression.COMPRESSION_LZW);
-					options.setQuality(100);
-					options.setCheckWorkbookDefaultFont(true);
-					options.setDefaultFont("Arial");
-					SheetRender sr = null;
-					Cells cells = workbookName.getCells();
-					Cell lastCell = cells.getLastCell();
-					int row = lastCell.getRow();
-					String imagePrintArea = "";
-					ByteArrayOutputStream outStream = null;
-					switch (name) {
-					case "sheet_1_image":
-						imagePrintArea = "A5:A31:B5:B31";
-						workbookName.getPageSetup().setPrintArea(imagePrintArea);
-						sr = new SheetRender(workbookName, options);
-						outStream = new ByteArrayOutputStream();
-						sr.toImage(0, outStream);
-						imageOutputStreamMap.put("sheet_1_image", outStream);
-						break;
-					}
-				}
-			}
+			
+			workbook.getWorksheets().get(0).getPageSetup().setPrintArea("A1:A27:B1:B27");
+			workbook.getWorksheets().get(0).getPageSetup().setLeftMargin(0);
+			workbook.getWorksheets().get(0).getPageSetup().setRightMargin(0);
+			workbook.getWorksheets().get(0).getPageSetup().setTopMargin(0);
+			workbook.getWorksheets().get(0).getPageSetup().setBottomMargin(0);
+			
+			ImageOrPrintOptions options = new ImageOrPrintOptions();
+			options.setOnePagePerSheet(true);
+			options.setImageType(ImageType.PNG);
+//			options.setImageType(ImageType.JPEG);
+			options.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			options.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+					RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+			options.setHorizontalResolution(100);
+			options.setVerticalResolution(100);
+			options.setTiffCompression(TiffCompression.COMPRESSION_LZW);
+			options.setQuality(100);
+			options.setCheckWorkbookDefaultFont(true);
+			options.setDefaultFont("Arial");
+			
+			SheetRender sr = new SheetRender(workbook.getWorksheets().get(0), options);
+			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+			sr.toImage(0, "C:\\Users\\RaviKumar(JAI)\\OneDrive - Formidium Corp\\Desktop\\output.png");
+			sr.toImage(0, outStream);
+			imageOutputStreamMap.put("sheet_1_image", outStream);
+//			for (int index = 0; index < sheetCount; index++) {
+//				Worksheet workbookName = workbook.getWorksheets().get(index);
+//				workbookName.getPageSetup().setLeftMargin(0);
+//				workbookName.getPageSetup().setRightMargin(0);
+//				workbookName.getPageSetup().setTopMargin(0);
+//				workbookName.getPageSetup().setBottomMargin(0);
+//				String name = workbookName.getName();
+//				Range range = workbookName.getCells().getMaxDisplayRange();
+//				if (range != null) {
+//					int tcols = range.getColumnCount();					
+//					ImageOrPrintOptions options = new ImageOrPrintOptions();
+//					options.setOnePagePerSheet(true);
+//					options.setImageType(ImageType.EMF);
+////					options.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+////					options.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+////							RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//					options.setHorizontalResolution(100);
+//					options.setVerticalResolution(100);
+////					options.setTiffCompression(TiffCompression.COMPRESSION_LZW);
+////					options.setQuality(100);
+//					options.setCheckWorkbookDefaultFont(true);
+////					options.setDefaultFont("Arial");
+//					options.setOnlyArea(true);
+//					SheetRender sr = null;
+//					Cells cells = workbookName.getCells();
+//					Cell lastCell = cells.getLastCell();
+//					int row = lastCell.getRow();
+//					String imagePrintArea = "";
+//					ByteArrayOutputStream outStream = null;
+//					switch (name) {
+//					case "sheet_1_image":
+//						try {
+//							imagePrintArea = "A5:A31:B5:B31";
+//							workbookName.getPageSetup().setPrintArea(imagePrintArea);
+//							sr = new SheetRender(workbookName, options);
+//							outStream = new ByteArrayOutputStream();
+//							sr.toImage(0, "C:\\Users\\RaviKumar(JAI)\\OneDrive - Formidium Corp\\Desktop\\aspose-lib\\sheet_image.emf");
+//							imageOutputStreamMap.put("sheet_1_image", outStream);
+//						} catch (Exception e) {
+//							e.printStackTrace();
+//						}
+//						break;
+//					}
+//				}
+//			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
