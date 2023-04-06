6f7598a2ffdebcd15f08185adedebb0c1c36675a
ravikmr474 <40664321+ravikmr474@users.noreply.github.com>
2023-04-06 15:14:04 +0530

    updated

--- src/main/java/com/aspose/demo/asposeimagecapture/FileMerge.java
+++ src/main/java/com/aspose/demo/asposeimagecapture/FileMerge.java
@@ -1,38 +1,67 @@
 package com.aspose.demo.asposeimagecapture;
 
 import java.awt.Dimension;
+import java.awt.HeadlessException;
 import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.util.IOUtils;
 import org.apache.poi.util.Units;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.aspose.cells.Cell;
+import com.aspose.cells.Cells;
 import com.aspose.cells.FontConfigs;
 import com.aspose.cells.ImageOrPrintOptions;
 import com.aspose.cells.ImageType;
+import com.aspose.cells.Range;
 import com.aspose.cells.SheetRender;
 import com.aspose.cells.Style;
-import com.aspose.cells.TextCapsType;
 import com.aspose.cells.TiffCompression;
 import com.aspose.cells.Workbook;
+import com.aspose.cells.Worksheet;
+import com.aspose.cells.WorksheetCollection;
+import com.aspose.words.FolderFontSource;
+import com.aspose.words.FontSettings;
+import com.aspose.words.FontSourceBase;
+import com.aspose.words.SystemFontSource;
 import com.spire.xls.PageSetup;
-import com.spire.xls.Worksheet;
+import com.spire.xls.core.spreadsheet.HTMLOptions;
 
 public class FileMerge {
 
@@ -43,44 +72,37 @@ public class FileMerge {
 		try {
 			for (XWPFParagraph p : doc.getParagraphs()) {
 				String text2 = p.getText().trim();
-				List<XWPFRun> runs = p.getRuns();
-				if (runs != null) {
-					for (XWPFRun r : runs) {
-						String text = r.getText(0);
-						if (text != null) {
-							BufferedImage image;
-							ByteArrayOutputStream bout;
-							ByteArrayInputStream bin;
-
-							InputStream imageInputStream = getInputStream(imageURLMap.get(text2));
-
-//							image = ImageIO.read(imageInputStream);
-//							Dimension dim = new Dimension(image.getWidth(), image.getHeight());
-//							// Dimension width
-//							double width = dim.getWidth();
-//							double height = dim.getHeight();
-//							double scaling = 1.0;
-//							if (width > 72 * 6.5)
-//								scaling = (72 * 6.5) / width;
-//							bout = new ByteArrayOutputStream();
-//							ImageIO.write(image, "png", bout);
-//							bout.flush();
-//							bin = new ByteArrayInputStream(bout.toByteArray());
-//							width = width * scaling;
-//							height = height * scaling;
-//							System.out.println("width: = " + width + " height" + height);
-
-							r.addPicture(imageInputStream, XWPFDocument.PICTURE_TYPE_EMF, "",
-									Units.toEMU(468.0), Units.toEMU(449.8666666666667));
-							
-							r.getCTR().getDrawingArray(0).getInlineArray(0).addNewCNvGraphicFramePr()
-									.addNewGraphicFrameLocks().setNoChangeAspect(true);
-							text = text.replace(text2, "");
-							r.setText(text, 0);
+					List<XWPFRun> runs = p.getRuns();
+					if (runs != null) {
+						for (XWPFRun r : runs) {
+							String text = r.getText(0);
+							if (text != null) {
+								BufferedImage image;
+								ByteArrayOutputStream bout;
+								ByteArrayInputStream bin;
+								InputStream imageInputStream = getInputStream(imageURLMap.get(text2));
+								image = ImageIO.read(imageInputStream);
+								Dimension dim = new Dimension(image.getWidth(), image.getHeight());
+								// Dimension width
+								double width = dim.getWidth();
+								double height = dim.getHeight();
+								double scaling = 1.0;
+								if (width > 72 * 6.5)
+									scaling = (72 * 6.5) / width;
+								bout = new ByteArrayOutputStream();
+								ImageIO.write(image, "png", bout);
+								bout.flush();
+								bin = new ByteArrayInputStream(bout.toByteArray());
+								r.addPicture(bin, XWPFDocument.PICTURE_TYPE_PNG, "", Units.toEMU(width*scaling),
+										Units.toEMU(height*scaling));
+								r.getCTR().getDrawingArray(0).getInlineArray(0).addNewCNvGraphicFramePr()
+										.addNewGraphicFrameLocks().setNoChangeAspect(true);
+								text = text.replace(text2, "");
+								r.setText(text, 0);
+							}
+							break;
 						}
-						break;
 					}
-				}
 			}
 		} catch (Exception e) {
 			System.out.println("Exception: " + e);
@@ -104,12 +126,12 @@ public class FileMerge {
 		Map<String, OutputStream> imageOutputStreamMap = new HashMap<String, OutputStream>();
 		try {
 
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			workbookFile.write(bos);
-			byte[] barray = bos.toByteArray();
-			InputStream is = new ByteArrayInputStream(barray);
-
-			Workbook workbook = new Workbook(is);
+//			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//			workbookFile.write(bos);
+//			byte[] barray = bos.toByteArray();
+//			InputStream is = new ByteArrayInputStream(barray);
+//
+//			Workbook workbook = new Workbook(is);
 //			FontConfigs.setDefaultFontName("Arial");
 //			workbook.calculateFormula();
 //			Style defaultStyle = workbook.getDefaultStyle();
@@ -125,49 +147,42 @@ public class FileMerge {
 //			WorksheetCollection worksheets = workbook.getWorksheets();
 //
 //			int sheetCount = worksheets.getCount();
-//			
-//			FontConfigs.setDefaultFontName("Arial");
-//			workbook.calculateFormula();
+			
+			Workbook workbook = new Workbook("C:\\Users\\RaviKumar(JAI)\\OneDrive - Formidium Corp\\Desktop\\My-excel.xlsx");
+			FontConfigs.setDefaultFontName("Arial");
+			workbook.calculateFormula();
 			Style defaultStyle = workbook.getDefaultStyle();
-			defaultStyle.getFont().setName("");
-//			wb.setDefaultStyle(s);
-//			defaultStyle.getFont().setName("Arial");
-//			defaultStyle.getFont().setSize(20);
-//			defaultStyle.setTextWrapped(true);
-//			defaultStyle.getFont().setBold(false);
-//			defaultStyle.setShrinkToFit(false);
-//			defaultStyle.getFont().setCapsType(TextCapsType.SMALL);
-//			defaultStyle.getFont().setArgbColor(10);
+
+			defaultStyle.getFont().setName("Arial");
+			defaultStyle.getFont().setSize(10);
+			defaultStyle.setTextWrapped(true);
+			defaultStyle.getFont().setBold(false);
 			workbook.setDefaultStyle(defaultStyle);
-
-//			Cell cell = workbook.get
-			workbook.getWorksheets().get(0).getPageSetup().setPrintArea("A5:A31:B5:B31");
-//			workbook.getWorksheets().get(0).getPageSetup().setLeftMargin(0);
-//			workbook.getWorksheets().get(0).getPageSetup().setRightMargin(0);
-//			workbook.getWorksheets().get(0).getPageSetup().setTopMargin(0);
-//			workbook.getWorksheets().get(0).getPageSetup().setBottomMargin(0);
-
+			
+			workbook.getWorksheets().get(0).getPageSetup().setPrintArea("A1:A27:B1:B27");
+			workbook.getWorksheets().get(0).getPageSetup().setLeftMargin(0);
+			workbook.getWorksheets().get(0).getPageSetup().setRightMargin(0);
+			workbook.getWorksheets().get(0).getPageSetup().setTopMargin(0);
+			workbook.getWorksheets().get(0).getPageSetup().setBottomMargin(0);
+			
 			ImageOrPrintOptions options = new ImageOrPrintOptions();
 			options.setOnePagePerSheet(true);
 			options.setImageType(ImageType.PNG);
 //			options.setImageType(ImageType.JPEG);
-//			options.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//			options.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-			options.setHorizontalResolution(150);
-			options.setVerticalResolution(150);
-//			options.setDesiredSize(0, 0);
-//			options.setTiffCompression(TiffCompression.COMPRESSION_NONE);
+			options.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			options.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+					RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+			options.setHorizontalResolution(100);
+			options.setVerticalResolution(100);
+			options.setTiffCompression(TiffCompression.COMPRESSION_LZW);
 			options.setQuality(100);
-//			options.setCheckWorkbookDefaultFont(false);
-			options.setDefaultFont("/usr/share/fonts/Arial");
-//			options.setDesiredSize(573, 526);
+			options.setCheckWorkbookDefaultFont(true);
+			options.setDefaultFont("Arial");
+			
 			SheetRender sr = new SheetRender(workbook.getWorksheets().get(0), options);
 			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-//			String excelFiles = "C:\\Users\\RaviKumar(JAI)\\OneDrive - Formidium Corp\\Desktop\\emfImage.png";
-
-//			sr.toImage(0, excelFiles);
+			sr.toImage(0, "C:\\Users\\RaviKumar(JAI)\\OneDrive - Formidium Corp\\Desktop\\output.png");
 			sr.toImage(0, outStream);
-
 			imageOutputStreamMap.put("sheet_1_image", outStream);
 //			for (int index = 0; index < sheetCount; index++) {
 //				Worksheet workbookName = workbook.getWorksheets().get(index);
@@ -205,6 +220,7 @@ public class FileMerge {
 //							workbookName.getPageSetup().setPrintArea(imagePrintArea);
 //							sr = new SheetRender(workbookName, options);
 //							outStream = new ByteArrayOutputStream();
+//							sr.toImage(0, "C:\\Users\\RaviKumar(JAI)\\OneDrive - Formidium Corp\\Desktop\\aspose-lib\\sheet_image.emf");
 //							imageOutputStreamMap.put("sheet_1_image", outStream);
 //						} catch (Exception e) {
 //							e.printStackTrace();
@@ -222,43 +238,66 @@ public class FileMerge {
 	}
 
 	public static Map<String, OutputStream> takeSnapshotFromExcelSpire(XSSFWorkbook workbookFile) throws IOException {
-
-		Map<String, OutputStream> imageOutputStreamMap = new HashMap<String, OutputStream>();
-
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		workbookFile.write(bos);
-		byte[] barray = bos.toByteArray();
-		InputStream is = new ByteArrayInputStream(barray);
-
-		com.spire.xls.Workbook workbook = new com.spire.xls.Workbook();
-
-		workbook.loadFromStream(is);
-
-		workbook.getConverterSetting().setXDpi(300);
-		workbook.getConverterSetting().setYDpi(300);
-
-//	        workbook.setDefaultFontName("Arial");
-//	        workbook.setDefaultFontSize(10);
-
-		workbook.getWorksheets();
-		Worksheet sheet = workbook.getWorksheets().get(0);
-		sheet.getCellRange(5, 1, 32, 2).getCellStyle().setIncludeFont(true);
 		
-		PageSetup pageSetup = sheet.getPageSetup();
-
-		// Set the page margins of bottom, left, right and top.
-		pageSetup.setBottomMargin(0);
-		pageSetup.setLeftMargin(0);
-		pageSetup.setRightMargin(0);
-		pageSetup.setTopMargin(0);
-		BufferedImage bufferedImage = sheet.toImage(5, 1, 32, 2);
-
-		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-		ImageIO.write(bufferedImage, "PNG", outStream);
-
-		imageOutputStreamMap.put("sheet_1_image", outStream);
-
+//		com.spire.xls.Workbook workbook = new com.spire.xls.Workbook();	
+//		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//		workbookFile.write(bos);
+//		byte[] barray = bos.toByteArray();
+//		InputStream is = new ByteArrayInputStream(barray);
+//		workbook.loadFromStream(is);
+//
+//		com.spire.xls.Worksheet sheet = workbook.getWorksheets().get(0);
+//
+//		HTMLOptions options = new HTMLOptions();	
+//		ByteArrayOutputStream outputStreamHTML = new ByteArrayOutputStream();
+//		sheet.saveToHtml(outputStreamHTML, options);
+////		sheet.saveToHtml("src/main/resources/htmlFile.html", options);
+//		try {
+//			uploadFile("htmlFile.html", outputStreamHTML);
+//			downloadFile();
+//		} catch (IOException e1) {
+//			e1.printStackTrace();
+//		}
+		
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		
+		ChromeOptions options1 = new ChromeOptions();
+		options1.addArguments("--remote-allow-origins=*");
+		WebDriver driver = null;
+		try {
+			driver = new ChromeDriver(options1);
+			driver.get("https://s3.amazonaws.com/seamlessserver/signinlogo/fsimages/htmlFile.html");
+			driver.manage().window().maximize();
+		} catch (Exception e) {
+			System.out.println("MEsage:-- "+e.getMessage());
+			e.printStackTrace();
+		}
+		
+//		int height = 814;
+//		try {
+//			width = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+//			height = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight()-50;
+//		} catch (HeadlessException e) {
+//			e.printStackTrace();
+//		}
+        
+        // Get the entire page screenshot
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        FileUtils.copyFile(screenshot, outputStream);
+        InputStream isFromFirstData = new ByteArrayInputStream(((ByteArrayOutputStream) outputStream).toByteArray());
+        
+        BufferedImage image = ImageIO.read(isFromFirstData);
+        
+        
+        BufferedImage croppedImage = image.getSubimage(10, 70, 696, 694);	
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();;
+        ImageIO.write(croppedImage, "png", baos);
+        Map<String, OutputStream> imageOutputStreamMap = new HashMap<String, OutputStream>();
+        imageOutputStreamMap.put("sheet_1_image", baos);
+        System.out.println("Done...done again");
+        driver.quit();
 		return imageOutputStreamMap;
 	}
-
+	
 }
